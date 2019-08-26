5922f6492756f6955417218acc8749d36d531490
huangbiao <huangbiao@npnets.com>
2019-08-26 14:37:59 +0800

    auto update

--- src/main/index.js
+++ src/main/index.js
@@ -5,17 +5,20 @@ import {
   Menu
 } from 'electron'
 import path from 'path'
+const {
+  autoUpdater
+} = require('electron-updater')
 
 if (process.env.NODE_ENV !== 'development') {
   global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
-let appTray = null;
 const winURL = process.env.NODE_ENV === 'development' ?
   `http://localhost:9080` :
   `file://${__dirname}/index.html`
 
+const feedUrl = `https://mius-1252282306.cos.ap-shanghai.myqcloud.com/${process.platform}`;
 
 function createWindow() {
   Menu.setApplicationMenu(null)
@@ -28,9 +31,10 @@ function createWindow() {
     resizable: true, //窗口是否可以改变尺寸
     show: false, //窗口创建的时候是否显示
     frame: false,
-    useContentSize:true,
+    useContentSize: true,
     webPreferences: {
-      webSecurity: false
+      webSecurity: false,
+      nodeIntegration: true
     },
   })
   mainWindow.loadURL(winURL)
@@ -48,11 +52,51 @@ function createWindow() {
     mainWindow.webContents.send('unmaximize', 'unmaximize')
   })
 }
+// 检测更新
+let checkForUpdates = () => {
+  autoUpdater.setFeedURL(feedUrl);
+
+  autoUpdater.on('error', function (message) {
+    mainWindow.webContents.send('message', {
+      "message":  'error'
+    })
+  });
+  autoUpdater.on('checking-for-update', function (message) {
+    mainWindow.webContents.send('message', {
+      "message": 'checking-for-update'
+    })
+  });
+  autoUpdater.on('update-available', function (message) {
+    mainWindow.webContents.send('message', {
+      "message": 'update-available'
+    })
+  });
+  autoUpdater.on('update-not-available', function (message) {
+    mainWindow.webContents.send('message', {
+      "message": 'update-not-available'
+    })
+  });
+
+  autoUpdater.on('download-progress', function (progressObj) {
+    mainWindow.webContents.send('message',  {
+      "message": 'download-progress',
+      "data": progressObj
+    })
+  })
+  autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+    mainWindow.webContents.send('message', {
+      "message": 'is-update-now',
+      "data": event
+    })
+  });
+
+  autoUpdater.checkForUpdates();
+};
 
 app.on('ready', () => {
   createWindow()
+  setTimeout(checkForUpdates, 1000);
 })
-
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
@@ -68,12 +112,18 @@ app.on('activate', () => {
 ipcMain.on('min', e => {
   mainWindow.minimize()
 });
+
 ipcMain.on('max', e => {
   if (mainWindow.isMaximized()) {
     mainWindow.unmaximize()
-    
+
   } else {
     mainWindow.maximize()
   }
 });
+
 ipcMain.on('close', e => mainWindow.close());
+
+ipcMain.on('update', (e, arg) => {
+  //autoUpdater.quitAndInstall();
+})
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -5,8 +5,41 @@
 </template>
 
 <script>
+  import {
+    ipcRenderer as ipc
+  } from 'electron';
+
   export default {
-    name: 'm-music'
+    name: 'm-music',
+    methods: {
+    },
+    mounted() {
+      let that = this
+      ipc.on('message', (event, {
+        message,
+        data
+      }) => {
+        console.log(message)
+        switch (message) {
+          case 'update-available':
+            that.$Message.info('正在检查更新');
+            break;
+          case 'error':
+            that.$Message.error('检查更新失败');
+            break;
+          case 'is-update-now':
+            that.$Modal.confirm({
+              title: '更新提示',
+              content: '<p>检测到新版本，是否立即更新？</p>',
+              okText: "更新",
+              onOk: () => {
+                ipc.send("update");
+              },
+            });
+            break;
+        }
+      });
+    }
   }
 </script>
 
